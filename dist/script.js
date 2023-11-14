eaf70e0b86ce3cd34e1a682befea98484b3d838a
unknown <rifkisyarif6@gmail.com>
2023-11-14 09:25:20 +0700

    webRifki

--- dist/script.js
+++ dist/script.js
@@ -11,10 +11,13 @@ window.onscroll = function () {
 };
 //hamburger
 const hamburger = document.querySelector("#hamburger");
-const navmenu = document.querySelector('#nav-menu')
+const navmenu = document.querySelector("#nav-menu");
 
 hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("hamburger-active");
-  navmenu.classList.toggle('hidden');
-
+  navmenu.classList.toggle("hidden");
 });
+
+import AOS from "aos";
+import "aos/dist/aos.css";
+AOS.init();
